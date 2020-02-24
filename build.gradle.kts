3cb879e7bcb38dd8fbd4c180f568351638b5d695
corneil <corneil.duplessis@gmail.com>
2020-02-24 17:05:14 +0200

    added visualization

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,23 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
+buildscript {
+    repositories {
+        mavenLocal()
+        maven {
+            url = uri("https://plugins.gradle.org/m2/")
+        }
+        mavenCentral()
+    }
+    dependencies {
+        classpath("io.jumpco.kfsm.open:kfsm-viz-plugin:1.0.3")
+    }
+}
 plugins {
     id("org.springframework.boot") version "2.2.4.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     kotlin("jvm") version "1.3.61"
     kotlin("plugin.spring") version "1.3.61"
 }
+apply(plugin = "io.jumpco.open.kfsm.viz-plugin")
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
@@ -28,7 +40,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("io.jumpco.open:kfsm-jvm:1.0.1-SNAPSHOT")
+    implementation("io.jumpco.open:kfsm-jvm:1.0.1")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("com.h2database:h2:1.4.200")
@@ -46,5 +58,19 @@ tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = "1.8"
+    }
+}
+
+val assemble by tasks.existing {
+    dependsOn("generateFsmViz")
+}
+
+configure<io.jumpco.open.kfsm.gradle.VizPluginExtension> {
+    fsm("TurnstileFSM") {
+        outputFolder = file("generated")
+        input = file("src/main/kotlin/com/example/kfsm/Turnstile.kt")
+        isGeneratePlantUml = true // Required default is false
+        isGenerateAsciidoc = true // Required default is false
+        output = "turnstile"
     }
 }
